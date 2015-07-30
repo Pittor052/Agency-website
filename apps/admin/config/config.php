f17cc275753c99ed75a30096037210773e0166ac
Nikolay Yotsov <karboratorr@gmail.com>
2015-07-30 23:06:03 +0300

    fixed untracked files

--- apps/admin/config/config.php
+++ apps/admin/config/config.php
@@ -5,7 +5,7 @@ return new \Phalcon\Config(array(
 		'adapter'  => 'Mysql',
 		'host'     => 'localhost',
 		'username' => 'root',
-		'password' => '',
+		'password' => 'aaa',
 		'name'     => 'blog',
 	),
 	'application' => array(
