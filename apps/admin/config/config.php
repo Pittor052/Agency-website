3bf1800117cd72039ce1ca54854d02078ee53d06
Nikolay Yotsov <karboratorr@gmail.com>
2015-07-30 23:07:23 +0300

    test again

--- apps/admin/config/config.php
+++ apps/admin/config/config.php
@@ -5,7 +5,7 @@ return new \Phalcon\Config(array(
 		'adapter'  => 'Mysql',
 		'host'     => 'localhost',
 		'username' => 'root',
-		'password' => 'aaa',
+		'password' => '',
 		'name'     => 'blog',
 	),
 	'application' => array(
