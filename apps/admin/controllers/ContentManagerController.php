429bbf2b4f6cf82f41a9e9683c3a086a778c9f2c
Pittor052 <lazar_off@yahoo.com>
2015-08-06 01:12:43 +0300

    merge conflicts

---
+++ apps/admin/controllers/ContentManagerController.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lazaroff
+ * Date: 15-7-29
+ * Time: 16:49
+ */
+
+namespace Bolar\Admin\Controllers;
+
+
+use Bolar\Admin\Controllers\ControllerBase;
+use Bolar\Frontend\Models\Contact;
+use Bolar\Frontend\Models\ContentManager;
+use Bolar\Frontend\Models\Gallery;
+use Bolar\Frontend\Models\Products;
+use Phalcon\Mvc\Model;
+
+
+class ContentManagerController extends ControllerBase
+{
+    //Home page
+    public function createAction()
+    {
+        $this->view->disable();
+        if ($this->request->isPost()) {
+            $contentManagerModel = new ContentManager();
+            if (empty($contentManagerModel->save($this->request->getPost()))) {
+                $contentManagerModel->setErr();
+                return $this->responseHandling();
+            }
+            return $this->responseHandling('Success');
+        }
+    }
+
+    public function editAction($id)
+    {
+        $this->view->disable();
+        if ($this->request->isPost()) {
+            $contentManagerModel = ContentManager::findFirst("id = '$id'");
+            if (empty($contentManagerModel->save($this->request->getPost()))) {
+                $contentManagerModel->setErr();
+                return $this->responseHandling();
+            }
+            return $this->responseHandling('Success');
+        }
+    }
+
+    public function deleteAction($id)
+    {
+        $this->view->disable();
+        if ($this->request->isPost()) {
+            $contentManagerModel = ContentManager::findFirst("id = '$id'");
+            $contentManagerModel->delete();
+            return $this->responseHandling('Success');
+        }
+    }
+}
---
+++ apps/admin/views/layouts/forms/contentManagerForm.volt
@@ -0,0 +1,24 @@
+<div class="box box-primary">
+    <form role="form" action="/admin/contnet-manager/create" method="POST" enctype="multipart/form-data">
+        <div class="box-body">
+            <div class="form-group">
+                <label for="file">Избери файл</label>
+                <input type="file" id="file" name="file">
+
+                <div class="form-group"><br>
+                    <label for="category">Изберете страница!</label>
+                    <select class="form-control" name="page_type" id="category">
+                        <option value="home">Home</option>
+                        <option value="about">About</option>
+                        <option value="services">Services</option>
+                    </select>
+                </div>
+            </div>
+            {{ partial("layouts/components/ckEditor") }}
+        </div>
+        <!-- /.box-body -->
+        <div class="box-footer">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </form>
+</div>
