b7d57f5bda20215b5915a81202a3d612d61694d1
Pittor052 <lazar_off@yahoo.com>
2015-08-09 16:52:00 +0300

    more info working for admin->products

--- apps/admin/views/products/index.volt
+++ apps/admin/views/products/index.volt
@@ -2,35 +2,38 @@
     <div class="container">
         <div class="row">
             {% if products and products is iterable %}
-                {% for product in products %}
-                    <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+            {% for product in products %}
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
                 <span class="">
                 <?php echo $product->getName(); ?>
             </span>
-                        <a class="thumbnail" href="">
-                            {% if product.getGallery() %}
-                                <img class="img-responsive" width="200" height="200"
-                                     src="<?php echo $product->getGallery()->getName()?>" alt="">
-                            {% else %}
-                                <img class="img-responsive" width="200" height="200"
-                                     src="/img/system/default_product.jpg" alt="">
-                            {% endif %}
-                        </a>
+                <a class="thumbnail" href="">
+                    {% if product.getGallery() %}
+                    <img class="img-responsive" width="200" height="200"
+                         src="<?php echo $product->getGallery()->getName()?>" alt="">
+                    {% else %}
+                    <img class="img-responsive" width="200" height="200"
+                         src="/img/system/default_product.jpg" alt="">
+                    {% endif %}
+                </a>
                 <span>
                 <?php echo substr($product->getDescription(),0, 40); ?>
                 <br><a href="http://store.dev/admin/products/{{ product.id }}">read more...</a>
             </span>
-                    </div>
-                {% endfor %}
+            </div>
+            {% endfor %}
 
             {% elseif products %}
-                {% if products.getGallery() %}
-                    <img class="img-responsive" width="200" height="200"
-                         src="{{ products.getGallery().getName() }}" alt="">
-                {% else %}
-                    <img class="img-responsive" width="200" height="200"
-                         src="/img/system/default_product.jpg" alt="">
-                {% endif %}
+            {% if products.getGallery() %}
+            <img class="img-responsive" width="200" height="200"
+                 src="{{ products.getGallery().getName() }}" alt="">
+            {% else %}
+            <img class="img-responsive" width="200" height="200"
+                 src="/img/system/default_product.jpg" alt="">
+            {% endif %}
+            <p> <h4>Описание:</h4> {{ products.getDescription() }} </p>
+            <p> <h4>В наличност:</h4> {{ products.getAvailable() }} </p>
+            <p> <h4>Цена:</h4> {{ products.getPrice() }} </p>
             {% endif %}
         </div>
     </div>
--- apps/admin/views/products/index.volt.php
+++ apps/admin/views/products/index.volt.php
@@ -2,35 +2,38 @@
     <div class="container">
         <div class="row">
             <?php if ($products && (is_array($products) || ($products) instanceof Traversable)) { ?>
-                <?php foreach ($products as $product) { ?>
-                    <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+            <?php foreach ($products as $product) { ?>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
                 <span class="">
                 <?php echo $product->getName(); ?>
             </span>
-                        <a class="thumbnail" href="">
-                            <?php if ($product->getGallery()) { ?>
-                                <img class="img-responsive" width="200" height="200"
-                                     src="<?php echo $product->getGallery()->getName()?>" alt="">
-                            <?php } else { ?>
-                                <img class="img-responsive" width="200" height="200"
-                                     src="/img/system/default_product.jpg" alt="">
-                            <?php } ?>
-                        </a>
+                <a class="thumbnail" href="">
+                    <?php if ($product->getGallery()) { ?>
+                    <img class="img-responsive" width="200" height="200"
+                         src="<?php echo $product->getGallery()->getName()?>" alt="">
+                    <?php } else { ?>
+                    <img class="img-responsive" width="200" height="200"
+                         src="/img/system/default_product.jpg" alt="">
+                    <?php } ?>
+                </a>
                 <span>
                 <?php echo substr($product->getDescription(),0, 40); ?>
                 <br><a href="http://store.dev/admin/products/<?php echo $product->id; ?>">read more...</a>
             </span>
-                    </div>
-                <?php } ?>
+            </div>
+            <?php } ?>
 
             <?php } elseif ($products) { ?>
-                <?php if ($products->getGallery()) { ?>
-                    <img class="img-responsive" width="200" height="200"
-                         src="<?php echo $products->getGallery()->getName(); ?>" alt="">
-                <?php } else { ?>
-                    <img class="img-responsive" width="200" height="200"
-                         src="/img/system/default_product.jpg" alt="">
-                <?php } ?>
+            <?php if ($products->getGallery()) { ?>
+            <img class="img-responsive" width="200" height="200"
+                 src="<?php echo $products->getGallery()->getName(); ?>" alt="">
+            <?php } else { ?>
+            <img class="img-responsive" width="200" height="200"
+                 src="/img/system/default_product.jpg" alt="">
+            <?php } ?>
+            <p> <h4>Описание:</h4> <?php echo $products->getDescription(); ?> </p>
+            <p> <h4>В наличност:</h4> <?php echo $products->getAvailable(); ?> </p>
+            <p> <h4>Цена:</h4> <?php echo $products->getPrice(); ?> </p>
             <?php } ?>
         </div>
     </div>
