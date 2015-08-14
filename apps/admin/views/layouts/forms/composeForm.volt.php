87606cf6133fb14d3107715a3648d927a5ca60ac
Pittor052 <lazar_off@yahoo.com>
2015-08-14 15:49:24 +0300

    compose footer buttons

--- apps/admin/views/layouts/forms/composeForm.volt.php
+++ apps/admin/views/layouts/forms/composeForm.volt.php
@@ -3,7 +3,7 @@
         <h3 class="box-title">Compose New Message</h3>
     </div>
     <form role="form" action="/admin/mailbox/send" method="POST" enctype="multipart/form-data">
-        
+
         <!-- /.box-header -->
         <div class="box-body">
             <div class="form-group">
@@ -167,7 +167,21 @@
             <div class="form-group">
                 <textarea id="#compose-textarea" class="form-control" style="height: 300px"></textarea>
             </div>
+            <div class="form-group">
+                <div class="btn btn-default btn-file">
+                    <i class="fa fa-paperclip"></i> Attachment
+                    <input type="file" name="attachment" />
+                </div>
+                <p class="help-block">Max. 32MB</p>
+            </div>
         </div>
+        <div class="box-footer">
+            <div class="pull-right">
+                <button class="btn btn-default"><i class="fa fa-pencil"></i> Draft</button>
+                <button type="submit" class="btn btn-primary"><i class="fa fa-envelope-o"></i> Send</button>
+            </div>
+            <button class="btn btn-default"><i class="fa fa-times"></i> Discard</button>
+        </div><!-- /.box-footer -->
         <!-- /.box-footer -->
     </form>
 </div>
